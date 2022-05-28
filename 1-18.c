3aebcc0021d99d653105ead83ad5196e33c9c633
Xenoxide <tayeemmachine@gmail.com>
2022-05-28 00:56:02 -0400

    funny

---
+++ 1-18.c
@@ -0,0 +1,41 @@
+// Remove trailing blanks and newlines from each line, and delete blank lines
+
+#define MAXLEN 500
+
+#include <stdio.h>
+
+// Does that and returns whether the line is blank
+int removeTrailing(char s[MAXLEN], char out[MAXLEN])
+{
+    int i;
+    for (i = 0; i != '\0'; i++)
+    {
+        
+    }
+}
+
+int main(void)
+{
+    int i, c, l;
+    i = 0;
+    l = 0;
+    char a[MAXLEN];
+    char b[MAXLEN];
+ 
+    while ((c = getchar()) != EOF)
+    {
+        if (c == '\n' || c == '\n')
+        {
+            a[i] = '\0';
+            if (!removeTrailing(a, b))
+                printf("%s\n", b);
+            i = 0;
+        } 
+        else
+        {
+            if (!(i >= MAXLEN))
+                a[i] = c;
+            i++;
+        }
+    }
+}
